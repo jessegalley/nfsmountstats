fa38cfa9b5f0e9c91bde41fef63e1ae53d40bf83
jesse galley <jesse.galley@gmail.com>
2024-12-12 07:11:48 -0400

    added cache examples

--- examples/cache/cache.go
+++ examples/cache/cache.go
@@ -8,9 +8,7 @@ import (
 )
 
 func main() {
-  // simply gather the current values for read/write throughput and ops 
-  // counters and print them. nothing fancy here.
-
+  // gather and calculate cache related stats for hitrate 
   mountstats, err := nfsmountstats.NewMountstats()
   if err != nil {
     log.Fatal(err)
@@ -20,7 +18,8 @@ func main() {
   // we don't care about, so first we'll grab just the NFS ones.
   nfsmounts := mountstats.GetNFSMountMap()
   
-  fmt.Printf("%-20s\t%-9s%-9s%-9s%-9s\n", "mountpoint", "readOps", "writeOps", "readKB", "writeKB")
+  fmt.Println("----------------Data Cache Stats--------------------------")
+  fmt.Printf("%-20s\t%-9s%-9s%-9s%-9s\n", "mountpoint", "readKB", "serverKB", "directKB", "hitrate" )
   fmt.Println("----------------------------------------------------------")
 
   // nfsmounts is a map[string]*MountDevice keyed on the path to the 
@@ -29,24 +28,53 @@ func main() {
   for mountpoint, mount := range nfsmounts {
     // fmt.Println(mount.Device)
 
-    // it's important to get the bytes and ops from the "per-ops" stats 
-    // as these are what's actually on the wire.
-    readStats := mount.NFSInfo.RPCOpStats["READ"]
-    writeStats := mount.NFSInfo.RPCOpStats["WRITE"]
+    // nfs cache stats come from the bytes counters instead of the per-ops 
+    // counters. there are different stats for the data and attribute caches 
+    
+    // NormalReadBytes represents the actual amount of bytes read from the 
+    // fs at the application layer by actual read() syscalls 
+    applicationBytesRead := mount.NFSInfo.Bytes.NormalReadBytes
 
-    cacheInvals := mount.NFSInfo.Events.AttrInvalidates
-    // READ and WRITE operations are generally all _file_ related ops, so 
-    // it's the majority of what we care about here. you need to sum both 
-    // bytes fields for each op to get total throughput
-    // for metadata ops, summ up the oter ops like GETATTRS SETATTRS etc 
-    readBytes := readStats.BytesSent + readStats.BytesReceived
-    writeBytes := writeStats.BytesSent + writeStats.BytesReceived
+    // ServerReadBytes represents the number of bytes read from the NFS server 
+    serverBytesRead := mount.NFSInfo.Bytes.ServerReadBytes
+    // DirectReadBytes represents the number of bytes read in O_DIRECT mode 
+    // but for some reason this is almost always 0, even when doing O_DIRECT reads 
+    directBytesRead := mount.NFSInfo.Bytes.DirectReadBytes
 
-    // ops counters are simple
-    readOps := readStats.Operations
-    writeOps := writeStats.Operations
+    var ratio float64
+    if applicationBytesRead != 0 {
+      // to calculate the data cache hitrate, we'll find the delta of bytes read 
+      // at the application layer vs from the NFS sserver  
+      clientBytesRead := serverBytesRead - directBytesRead
+      ratio = (float64(applicationBytesRead - clientBytesRead) * 100) / float64(applicationBytesRead)
+    }
 
-    fmt.Printf("%-20s\t%-9d%-9d%-9d%-9d%-9d\n", mountpoint, readOps, writeOps, readBytes/1024, writeBytes/1024, cacheInvals)
+    fmt.Printf("%-20s\t%-9d%-9d%-9d%-94.2f\n", mountpoint, applicationBytesRead/1024, serverBytesRead/1024, directBytesRead/1024, ratio)
   }
+
+  fmt.Println("----------------Attribute Cache Stats---------------------")
+  fmt.Printf("%-20s\t%-10s%-10s%-10s%-10s\n", "mountpoint", "vfsOpen", "inReval", "attInval", "dataReval" )
+  fmt.Println("----------------------------------------------------------")
+
+  for mountpoint, mount := range nfsmounts {
+
+    // the number of times a file or dir was open()'d at the linux VFS layer
+    vfsOpens := mount.NFSInfo.Events.VfsOpen
+
+    // the number of times a GETATTR forced attribute revalidation from the NFS server 
+    // these are basically attribute cache misses, I believe the attribute cache has 
+    // a TTL as little as 3 seconds, so there is often lots of these.
+    inodeReval := mount.NFSInfo.Events.InodeRevalidates
+    // i'm not exactly sure of when attrInvals happen other than the above revals 
+    attrInval := mount.NFSInfo.Events.AttrInvalidates
+    // the number times an inode has had it's cached data thrown out 
+    dataReval := mount.NFSInfo.Events.DataInvalidates
+
+    fmt.Printf("%-20s\t%-10d%-10d%-10d%-10d\n", mountpoint, vfsOpens, inodeReval, attrInval, dataReval)
+  }
+
+
+
+
 }
 
